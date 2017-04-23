1456836b61242462252e9628d6b98553f7c62aef
Alexander Rookey <atrookey@gmail.com>
2017-04-23 17:34:57 -0400

    rewrite using angular 2 quickstart

---
+++ src/app/app.component.js
@@ -0,0 +1,63 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require("@angular/core");
+var core_2 = require("@angular/core");
+var http_1 = require("@angular/http");
+require("rxjs/add/operator/toPromise");
+var Question = (function () {
+    function Question() {
+    }
+    return Question;
+}());
+exports.Question = Question;
+var QuestionsService = (function () {
+    function QuestionsService(http) {
+        this.http = http;
+        this.questionsUrl = 'questions.json'; // URL to web api
+    }
+    QuestionsService.prototype.getQuestions = function () {
+        return this.http.get(this.questionsUrl)
+            .toPromise()
+            .then(function (response) { return response.json().data; })
+            .catch(this.handleError);
+    };
+    QuestionsService.prototype.handleError = function (error) {
+        console.error('An error occurred', error);
+        return Promise.reject(error.message || error);
+    };
+    return QuestionsService;
+}());
+QuestionsService = __decorate([
+    core_2.Injectable(),
+    __metadata("design:paramtypes", [http_1.Http])
+], QuestionsService);
+exports.QuestionsService = QuestionsService;
+var AppComponent = (function () {
+    function AppComponent(questionsService) {
+        this.questionsService = questionsService;
+        this.getHeroes();
+    }
+    AppComponent.prototype.getHeroes = function () {
+        var _this = this;
+        this.questionsService.getQuestions().then(function (questions) { return _this.questions = questions; });
+    };
+    return AppComponent;
+}());
+AppComponent = __decorate([
+    core_1.Component({
+        selector: 'my-app',
+        providers: [QuestionsService],
+        templateUrl: 'question_list.html'
+    }),
+    __metadata("design:paramtypes", [QuestionsService])
+], AppComponent);
+exports.AppComponent = AppComponent;
+//# sourceMappingURL=app.component.js.map
---
+++ src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+
+export class Question {
+  _id: number;
+  question: string;
+  answers: any[];
+  correct: number;
+}
+
+@Injectable()
+export class QuestionsService {
+  private questionsUrl = 'questions.json';  // URL to web api
+  constructor(private http: Http) { }
+
+  getQuestions(): Promise<Question[]> {
+    return this.http.get(this.questionsUrl)
+        .toPromise()
+        .then(response => response.json().data as Question[])
+        .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error);
+    return Promise.reject(error.message || error);
+  }
+}
+
+
+@Component({
+  selector: 'my-app',
+  providers: [QuestionsService],
+  templateUrl: 'question_list.html'
+})
+export class AppComponent  {
+
+  questions: Question[];
+
+  constructor(private questionsService: QuestionsService) {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.questionsService.getQuestions().then(questions => this.questions = questions);
+  }
+}
