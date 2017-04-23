5b5b10de9abb6f94aef1943493cd3e59e212917f
Alexander Rookey <atrookey@gmail.com>
2017-04-23 18:24:44 -0400

    Operational again

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule }    from '@angular/http';
 
 import { AppComponent, QuestionsService }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule,  HttpModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ],
-  providers: [ QuestionsService ]
+  providers: [ QuestionsService ],
 })
 export class AppModule { }
